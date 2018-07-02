d0977041495e3ef06e3bcd1a92aca55472fdded0
Jacob Coleman <Jacob.Coleman@leidos.com>
2018-07-02 13:08:17 -0400

    Fixes to twitter block

--- src/Plugin/Block/apirendererTwitterBlock.php
+++ src/Plugin/Block/apirendererTwitterBlock.php
@@ -21,39 +21,67 @@ class apirendererTwitterBlock extends BlockBase implements BlockPluginInterface 
      * {@inheritdoc}
      */
     public function build() {
-        $html = 'test';
+        $config = $this->getConfiguration();
+        $return = 'test';
         $tweetlist = '';
         $data = \Drupal::state()->get('apirenderer.twitterData') ?: FALSE;
         if ($data) {
-            foreach ($data as $tweet) {
-                $type = 'twitter';
-                $icon = $tweet['user']['profile_image_url_https'];
-                $name = $tweet['user']['screen_name'];
-                $id = $tweet['id_str'];
-                $content = $this->formattedTweet($tweet['text']);
-                $url = "https://twitter.com/statuses/$id";
-                $date = time_elapsed_string('@'. strtotime($tweet['created_at']));
-                $extras = array();
-                $quoted = !empty($tweet['quoted_status']) ? ($tweet['quoted_status']) : NULL;
-                if ($quoted) {
-                    $extras = array(
-                        'quotedStatus' => $quoted,
-                        'quotedUser' => $quoted['user']['name'],
-                        'quotedScreenName' => $quoted['user']['screen_name'],
-                        'quotedMessage' => $this->formattedTweet($quoted['text']),
-                        'quottedDate' => '@' . strtotime($quoted['created_at']),
-                    );
+            if (array_key_exists('user', $data[0])) {
+                $content = '';
+                foreach ($data as $num => $tweet) {
+                    $type = 'twitter';
+                    $icon = $tweet['user']['profile_image_url_https'];
+                    $name = $tweet['user']['screen_name'];
+                    $id = $tweet['id_str'];
+                    $message = $this->formattedTweet($tweet['text']);
+                    $url = "https://twitter.com/statuses/$id";
+                    $date = time_elapsed_string('@' . strtotime($tweet['created_at']));
+                    $extras = array();
+                    $quoted = !empty($tweet['quoted_status']) ? ($tweet['quoted_status']) : NULL;
+                    if ($quoted) {
+                        $extras = array(
+                            'quotedStatus' => $quoted,
+                            'quotedUser' => $quoted['user']['name'],
+                            'quotedScreenName' => $quoted['user']['screen_name'],
+                            'quotedMessage' => $this->formattedTweet($quoted['text']),
+                            'quottedDate' => '@' . strtotime($quoted['created_at']),
+                        );
+                    }
+                    $extras['realName'] = $tweet['user']['name'];
+                    $extras['favCount'] = $tweet['favorite_count'];
+                    $extras['retweetCount'] = $tweet['retweet_count'];
+                    $extras['retweetedStatusClass'] = !empty($tweet['retweeted_status'])?'retweeted':'';
+                    $renderable = [
+                        '#theme' => 'apirenderer-post',
+                        '#blocktype' => $type,
+                        '#icon' => $icon,
+                        '#name' => $name,
+                        '#content' => $message,
+                        '#url' => $url,
+                        '#date' => $date,
+                        '#extras' => $extras
+                    ];
+                    $content .= drupal_render($renderable);
+                    if ($num == $config['apirenderer_twitter_block_num_to_show']) {
+                        break;
+                    }
                 }
-                $extras['realName'] = $tweet['user']['name'];
-                $extras['favCount'] = $tweet['favorite_count'];
-                $extras['retweetCount'] = $tweet['retweet_count'];
-                $extras['retweetedStatusClass'] = !empty($tweet['retweeted_status'])?'retweeted':'';
+                $return = [
+                    '#theme' => 'apirenderer-base-block',
+                    '#blocktype' => 'twitter',
+                    '#content' => $content,
+                    '#name' => \Drupal::config('apirenderer.settings')->get('apirenderer_twitter_user_timeline'),
+                    '#url' => "https://twitter.com/" . $data[0]['user']['screen_name']
+                ];
+            }
+            else {
+                $return = array('#markup' => $data[0]['code'] . ": " . $data[0]['message']);
             }
         }
         else {
-            $html .= 'oh noooo';
+            $return .= array('#markup' => 'Twitter data missing!');
         }
-        return array('#markup' => $html);
+        return $return;
     }
     /**
      * {@inheritdoc}
@@ -63,7 +91,7 @@ class apirendererTwitterBlock extends BlockBase implements BlockPluginInterface 
         $config = $this->getConfiguration();
         //Add a select box of numbers from 1 to $max_items
         $form['apirenderer_twitter_block_num'] = array(
-            '#type' => 'select',
+            '#blocktype' => 'select',
             '#title' => t('Tweets To Show'),
             '#default_value' => isset($config['apirenderer_twitter_block_num_to_show']) ? $config['apirenderer_twitter_block_num_to_show'] : 1,
             '#options' => array_combine(range(1, $max_items), range(1, $max_items)),
@@ -74,98 +102,6 @@ class apirendererTwitterBlock extends BlockBase implements BlockPluginInterface 
     public function blockSubmit($form, FormStateInterface $form_state) {
         $this->configuration['apirenderer_twitter_block_num_to_show'] = $form_state->getValue('apirenderer_twitter_block_num');
     }
-    /**
-     * APIRENDERER TWITTER CONTENT
-     * 
-     * Generates block contents for the Twitter block.
-     * 
-     * Returns the formatted HTML.
-     */
-    function apirenderer_twitter_content() {
-        $html = '';
-        $tweetlist = '';
-        $data = \Drupal::state()->get('apirenderer.twitterData');
-        if ($data) {
-            foreach ($data as $tweets) {
-                $date = '@'. strtotime($tweets['created_at']);
-                $message = formattedTweet($tweets['text']);
-                $quoted = !empty($tweets['quoted_status']) ? ($tweets['quoted_status']) : NULL;
-                $quoteduser = $quoted['user']['name'];
-                $quotedscreen_name = $quoted['user']['screen_name'];
-                $quotedmessage = formattedTweet($quoted['text']);
-                $quoteddate = '';
-                if ($quoted) {
-                    $quoteddate = '@' . strtotime($quoted['created_at']);
-                }
-                // @FIXME
-    // theme() has been renamed to _theme() and should NEVER be called directly.
-    // Calling _theme() directly can alter the expected output and potentially
-    // introduce security issues (see https://www.drupal.org/node/2195739). You
-    // should use renderable arrays instead.
-    // 
-    // 
-    // @see https://www.drupal.org/node/2195739
-    // $tweetlist .= theme(
-    // 				'apirenderer_twitter_single',
-    // 				array(
-    // 					'tweet_id' => $tweets['id_str'],
-    // 					'avatar' => $tweets['user']['profile_image_url_https'],
-    // 					'name' => $tweets['user']['name'],
-    // 					'screen_name' => $tweets['user']['screen_name'],
-    // 					'message' => $message,
-    // 					'date' => time_elapsed_string($date),
-    // 					'fav_count' => $tweets['favorite_count'],
-    // 					//retweet variables //
-    // 					'retweet_count'=>$tweets['retweet_count'],
-    // 					'retweeted_status_class' => !empty($tweets['retweeted_status'])?'retweeted':'',
-    // 					'quotedstatus' => $quoted,
-    // 					'quoteduser' => $quoteduser,
-    // 					'quotedmessage' => $quotedmessage,
-    // 					'quotedscreen_name' => $quotedscreen_name,
-    // 					'quoteddate' => time_elapsed_string($quoteddate),
-    // 				)
-    // 			);
-
-            };
-            // @FIXME
-    // theme() has been renamed to _theme() and should NEVER be called directly.
-    // Calling _theme() directly can alter the expected output and potentially
-    // introduce security issues (see https://www.drupal.org/node/2195739). You
-    // should use renderable arrays instead.
-    // 
-    // 
-    // @see https://www.drupal.org/node/2195739
-    // $html = theme(
-    // 			'apirenderer_twitter_feed',
-    // 			array(
-    // 				'avatar' => $data[0]['user']['profile_image_url_https'],
-    // 				'name' => $data[0]['user']['name'],
-    // 				'screen_name' => $data[0]['user']['screen_name'],
-    // 				'content' => $tweetlist,
-    // 			)
-    // 		);
-
-        }
-        else {
-            // @FIXME
-    // theme() has been renamed to _theme() and should NEVER be called directly.
-    // Calling _theme() directly can alter the expected output and potentially
-    // introduce security issues (see https://www.drupal.org/node/2195739). You
-    // should use renderable arrays instead.
-    // 
-    // 
-    // @see https://www.drupal.org/node/2195739
-    // $html = theme(
-    // 			'apirenderer_error',
-    // 			array(
-    // 				'message' => 'No Twitter data was found! Please check your twitter app settings under the Api Renderer config screen.'
-    // 			)
-    // 		);
-
-        }
-        return $html;
-    }
-
     /**
      * FORMATTED TWEET FUNCTION
      *
